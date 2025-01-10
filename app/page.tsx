ccd4796b90d303bb7c93d62a6f2007bd9fff6132
richardybanez037 <richardybanez037@gmail.com>
2025-01-10 19:49:05 +0800

    test

--- app/page.tsx
+++ app/page.tsx
@@ -16,6 +16,7 @@ export default function Page() {
           <p
             className={`text-xl text-gray-800 md:text-3xl md:leading-normal ${lusitana.className} antialiased`}
           >
+            <h1>test</h1>
             <strong>Welcome to Acme.</strong> This is the example for the{" "}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
               Next.js Learn Course
